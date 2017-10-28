e0005cd08007a26177d5c1b6f3f6439a3eea2b65
CHlM3RA <33183465+CHlM3RA@users.noreply.github.com>
2017-10-28 21:37:33 +0200

    Update Sqlhandle.php

--- Sqlhandle.php
+++ Sqlhandle.php
@@ -1,14 +1,23 @@
 <?php
 
-class Sqlhandle extends PDO {
+/**
+ * Sqlhandle: A class to siplify the usage of the PDO class
+ *
+ * @autor Florian Heidebrecht
+ * edited by Philipp Caldwell
+ *
+ */
+
+class Sqlhandle extends PDO{
 
 	private $host;
 	private $dbName;
 	private $user;
 	private $password;
+
 	public $lastQuerry = '';
 
-	public function __construct($host = 'localhost', $dbName = '', $user = 'root', $password = '') {
+	public function __construct($host = 'localhost', $dbName = '', $user = 'root', $password = ''){
 		$this->host = $host;
 		$this->dbName = $dbName;
 		$this->user = $user;
@@ -17,132 +26,108 @@ class Sqlhandle extends PDO {
 		$this->conn();
 	}
 
-	public function setDbName($dbName) {
-		$this->dbName = $dbName;
+	public function setDbName($value){
+		$this->dbName = $value;
 		$this->conn();
 	}
 
-	private function conn() {
-		try {
+	private function conn(){
+		try{
 			parent::__construct(
-					'mysql:host=' . $this->host . ';dbname=' . $this->dbName . ';', $this->user, $this->password, array(PDO::ATTR_TIMEOUT => 1)
+				'mysql:host='.$this->host.';dbname='.$this->dbName.';',
+				$this->user,
+				$this->password,
+				array(PDO::ATTR_TIMEOUT => 1)
 			);
-		} catch (PDOException $exception) {
+		} catch (PDOException $exception){
 			return $exception;
 		}
 
 		return true;
 	}
 
-	private function doQuery($query, $mode = PDO::FETCH_COLUMN) {
+	private function doQuery($query, $mode = PDO::FETCH_COLUMN){
 		$obj = $this->query($query);
 
-		if($obj) {
+		if($obj){
 			return $obj->fetchAll($mode);
 		}
 
 		return NULL;
 	}
 
-	public function getDatabaseNames() {
+	public function getDatabaseNames(){
 		return $this->doQuery('SHOW DATABASES');
 	}
 
-	public function getTableNames() {
-		$data = ['others' => [$this->dbName]];
-		$query = $this->prepareQuery('SHOW TABLES IN $other', $data);
+	public function getTableNames(){
+		$data = [$this->dbName];
+		$query = $this->prePrepare('SHOW TABLES IN $other', $data);
 
 		return $this->doQuery($query);
 	}
 
-	public function tableExists($table) {
+	public function tableExists($table){
 		$tableNames = $this->getTableNames();
 
-		if(in_array(strtolower($table), $tableNames)) {
+		if(in_array(strtolower($table), $tableNames)){
 			return true;
 		}
 
 		return false;
 	}
 
-	public function getColumnNames($table) {
-		$data = ['tables' => [$table]];
-		$query = $this->prepareQuery('DESCRIBE $table', $data);
+	public function getColumnNames($table){
+		$data = [$table];
+		$query = $this->prePrepare('DESCRIBE $table', $data);
 
 		return $this->doQuery($query);
 	}
 
-	public function columExists($table, $column) {
+	public function columExists($table, $column){
 		$columnNames = $this->getColumnNames($table);
 
-		if(in_array(strtolower($column), $columnNames)) {
+		if(in_array(strtolower($column), $columnNames)){
 			return true;
 		}
 
 		return false;
 	}
 
-	public function prepare($query, $data = []) {
-		if(!$this->isNumeric($data)) {
-			$query = $this->prepareQuery($query, $data);
-		}
+	public function prepare($query, $data = []){
+		$query = $this->prePrepare($query, $data);
 
 		$obj = parent::prepare($query);
 		$this->lastQuerry = $query;
 
-		foreach($data as $key => $val) {
+		foreach($data as $key => $val){
 			$obj->bindValue($key + 1, $val);
 		}
 
-		if(!$obj->execute()) {
-			return NULL;
+		if(!$obj->execute()){
+			return $this->errorInfo();
 		}
 
 		return $obj->fetchAll(PDO::FETCH_ASSOC);
 	}
 
-	private function isNumeric($data) {
-		return array_keys($arr) === range(0, count($arr) - 1);
-	}
+	private function prePrepare($query, &$data){
+		$pattern = '/(?<!\$)\$(?!\$)/';
 
-	private function prepareQuery($query, &$data) {
-		if(isset($data['columns'])) {
-			$columnNames = array();
+		$numberOfMatches = preg_match_all($pattern, $query);
 
-			foreach($data['tables'] as $table) {
-				$columnNames = array_merge($columnNames, $this->getColumnNames($table));
-			}
-
-			$query = $this->prepareField('columns', $query, $data, $columnNames);
-		}
-
-		if(isset($data['tables'])) {
-			$query = $this->prepareField('tables', $query, $data);
-		}
-
-		if(isset($data['others'])) {
-			$query = $this->prepareField('others', $query, $data);
-		}
-
-		return $query;
-	}
-
-	private function prepareField($index, $query, &$data, $columnNames = []) {
-		foreach($data[$index] as $element) {
-			if($index == 'tables' and $this->tableExists($element)) {
-				$query = preg_replace('/(\$table)/', $element, $query, 1);
-			} else if($index == 'columns' and in_array(strtolower($element), $columnNames)) {
-				$query = preg_replace('/(\$column)/', $element, $query, 1);
-			} else if($index == 'others' and preg_match('/^(\w+)$/', $element)) {
-				$query = preg_replace('/(\$other)/', $element, $query, 1);
+		for($i = 0; $i < $numberOfMatches; $i++){
+			if(preg_match('/^\w+$/', $data[0])){
+				$query = preg_replace($pattern, array_shift($data), $query, 1);
+			} else{
+				return false;
 			}
 		}
 
-		unset($data[$index]);
+		str_replace('$$', '$', $query);
 
 		return $query;
 	}
-
 }
 
 ?>
